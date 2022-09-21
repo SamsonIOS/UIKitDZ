9f8f59ffe0241b50622a6fb6add3b711e9af5269
Sergei <sc.fod@ya.ru>
2022-09-21 20:25:28 +0300

    patternUIKit

---
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by coder on 21.09.2022.
+//
+
+import UIKit
+/// Начальний VC
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
