e1af33d8df37730ae7243a74605954342a7fd30b
Sergei <sc.fod@ya.ru>
2022-09-22 14:31:41 +0300

    dz3_Alert_part1

--- UIKitDZ/UIKitDZ/ViewController.swift
+++ UIKitDZ/UIKitDZ/ViewController.swift
@@ -9,7 +9,97 @@ import UIKit
 /// Начальний VC
 class ViewController: UIViewController {
 
+    let addictionButton = UIButton()
+    let guessButton = UIButton()
+    let sumLable = UILabel()
+    let guessLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .gray
+        buttons()
+        createLabels()
+        
+        addictionButton.addTarget(self, action: #selector(actionButtonSum(sender: )), for: .touchUpInside)
+        guessButton.addTarget(self, action: #selector(actionButtonGuess(sender: )), for: .touchUpInside)
+    }
+    
+    func createLabels() {
+        sumLable.numberOfLines = 0
+        sumLable.textColor = .white
+        sumLable.font = .systemFont(ofSize: 20)
+        sumLable.frame = CGRect(x: 0, y: 300, width: 240, height: 50)
+        sumLable.center.x = view.center.x
+        view.addSubview(sumLable)
+        
+        guessLabel.numberOfLines = 0
+        guessLabel.textColor = .white
+        guessLabel.font = .systemFont(ofSize: 20)
+        guessLabel.frame = CGRect(x: 0, y: 520, width: 240, height: 50)
+        guessLabel.center.x = view.center.x
+        view.addSubview(guessLabel)
+    }
+    
+    func buttons() {
+        addictionButton.setTitle("Сложение", for: .normal)
+        addictionButton.backgroundColor = .green
+        addictionButton.layer.cornerRadius = 30
+        addictionButton.frame = CGRect(x: 0, y: 190, width: 230, height: 100)
+        addictionButton.center.x = view.center.x
+        view.addSubview(addictionButton)
+        
+        guessButton.setTitle("Угадай число", for: .normal)
+        guessButton.backgroundColor = .green
+        guessButton.layer.cornerRadius = 30
+        guessButton.frame = CGRect(x: 80, y: 400, width: 230, height: 100)
+        view.addSubview(guessButton)
+    }
+    
+    @objc func actionButtonSum(sender: UIButton) {
+        
+        let alertForButton = UIAlertController(
+            title: "Математика",
+            message: "Введите числа для сложение",
+            preferredStyle: .alert)
+        
+        let actionButton = UIAlertAction(title: "Посчитай", style: .default) { _ in
+            
+            let firstTextField = alertForButton.textFields?.first?.text ?? "nil"
+            let secondTextField = alertForButton.textFields?.last?.text ?? "nil"
+            
+            let firstTextFieldInt = Int(firstTextField) ?? 0
+            let secondTextFieldInt = Int(secondTextField) ?? 0
+            let sumFirstAndSecondTextField = firstTextFieldInt + secondTextFieldInt
+            self.sumLable.text = "Сумма чисел равна: \(sumFirstAndSecondTextField)"
+            print(sumFirstAndSecondTextField)
+        }
+        
+        alertForButton.addTextField()
+        alertForButton.addTextField()
+        
+        alertForButton.addAction(actionButton)
+        present(alertForButton, animated: true)
+    }
+    @objc func actionButtonGuess(sender: UIButton) {
+        
+        let alertForButton = UIAlertController(
+            title: "Угадай число",
+            message: "Введите число от 1 до 5",
+            preferredStyle: .alert)
+        
+        let actionButton = UIAlertAction(title: "OK", style: .default) { _ in
+            let numberRandom = Int.random(in: 1...5)
+            let valueOfApp = alertForButton.textFields?.first?.text ?? "0"
+            
+            if numberRandom == Int(valueOfApp) {
+                self.guessLabel.text = "Ты угадал! \(valueOfApp)"
+            } else {
+                self.guessLabel.text = "Не угадал мое число - \(numberRandom)"
+            }
+        }
+        alertForButton.addTextField()
+        
+        alertForButton.addAction(actionButton)
+        present(alertForButton, animated: true)
     }
 }
