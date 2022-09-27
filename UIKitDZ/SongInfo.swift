180a14816a017748cd7c58786e0ba5b4496f2ab2
Sergei <sc.fod@ya.ru>
2022-09-27 23:19:33 +0300

    исправил замечания

---
+++ UIKitDZ/SongInfo.swift
@@ -0,0 +1,19 @@
+//
+//  SongInfo.swift
+//  UIKitDZ
+//
+//  Created by coder on 27.09.2022.
+//
+/// SongInfo - Информация о треке
+/// name - Название трека
+/// time - Время трека
+/// image - Фото песни
+/// artist - Исполнитель песни
+/// trackName - название песни которое мы загрузили
+struct SongInfo {
+    let name: String
+    let time: Int
+    let image: String
+    let artist: String
+    let trackName: String
+}
--- UIKitDZ/UIKitDZ/ListViewController.swift
+++ UIKitDZ/UIKitDZ/ListViewController.swift
@@ -6,10 +6,47 @@
 //
 
 import UIKit
-/// ListViewController - экран с музыкой
+/// ListViewController - экран со списком музыки
 class ListViewController: UIViewController {
+    // MARK: ButtonsOutlet
+    @IBOutlet weak var firstTrackButton: UIButton!
+    @IBOutlet weak var secondTrackButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    // MARK: Кнопка перехода на экран с первым треком
+    @IBAction func firstTrackButtonAction(_ sender: UIButton) {
+        goToPlayerViewController(song: SongInfo(
+            name: "Blue Monday",
+            time: 267,
+            image: "mondayphoto",
+            artist: "ORGY",
+            trackName: "track1"))
+    }
+    // MARK: Кнопка перехода на экран со вторым треком
+    @IBAction func secondTrackButtonAction(_ sender: UIButton) {
+        goToPlayerViewController(song: SongInfo(
+            name: "Cherry Pie",
+            time: 207,
+            image: "musicphoto",
+            artist: "Warrant",
+            trackName: "track2"))
+    }
+    // MARK: Переход на экран с треком, который мы выбрали
+    func goToPlayerViewController(song: SongInfo) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let songVC = storyboard.instantiateViewController(
+            withIdentifier: "vc2"
+        ) as? TrackViewController else { return }
+        songVC.song = SongInfo(
+            name: song.name,
+            time: song.time,
+            image: song.image,
+            artist: song.artist,
+            trackName: song.trackName
+        )
+        songVC.modalPresentationStyle = .formSheet
+        present(songVC, animated: true)
     }
 }
