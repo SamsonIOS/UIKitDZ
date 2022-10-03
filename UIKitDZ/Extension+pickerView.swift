7878df708457f286d0919edc688a25d087b6593b
Sergei <sc.fod@ya.ru>
2022-10-03 21:26:49 +0300

    сделал 11 дз

---
+++ UIKitDZ/Extension+pickerView.swift
@@ -0,0 +1,49 @@
+//
+//  Extention+pickerView.swift
+//  UIKitDZ
+//
+//  Created by coder on 03.10.2022.
+//
+
+import Foundation
+import UIKit
+/// Расширяем наш экран с таймером для часов минут и секунд
+extension TimerViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return hours.count
+        case 1:
+            return minutes.count
+        case 2:
+            return seconds.count
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(
+        _ pickerView: UIPickerView,
+        attributedTitleForRow row: Int,
+        forComponent component: Int) -> NSAttributedString? {
+            var timeForElements = ""
+        switch component {
+        case 0:
+            timeForElements = "\(hours[row]) h"
+        case 1:
+            timeForElements = "\(minutes[row]) min"
+        case 2:
+            timeForElements = "\(seconds[row]) s"
+        default:
+            break
+        }
+            let attributedString = NSAttributedString(
+                string: timeForElements,
+                attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+            return attributedString
+    }
+}
---
+++ UIKitDZ/UIKitDZ/TimerViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by coder on 21.09.2022.
+//
+
+import UIKit
+/// Экран с таймером
+class TimerViewController: UIViewController {
+    // MARK: IBOutlet
+    @IBOutlet weak var timerPickerView: UIPickerView!
+    @IBOutlet weak var signalForTimerButton: UIButton!
+    
+    // MARK: Public properties
+    let hours = Array(0...23)
+    let minutes = Array(0...59)
+    let seconds = Array(0...59)
+    
+    // MARK: Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setPicker()
+    }
+    // MARK: Public Methods
+    func setPicker() {
+        timerPickerView.delegate = self
+        timerPickerView.dataSource = self
+        signalForTimerButton.layer.cornerRadius = 10
+    }
+}
