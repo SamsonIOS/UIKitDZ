8d7d7ee620fd9d6ae187e0e03261d5702be9a679
Sergei <sc.fod@ya.ru>
2022-10-17 09:44:32 +0300

    dz23_24 done

---
+++ UIKitDZ/UIKitDZ/StoryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  UIKitDZ
+//
+//  Created by coder on 21.09.2022.
+//
+
+import UIKit
+
+/// Экран со светофором сделанным с помощью сториборда
+final class StoryViewController: UIViewController {
+
+    // MARK: Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
